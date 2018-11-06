705b24f1d758ba6d921ffbfc589cb77f8d5bf231
asariy <asariy@outlook.com>
2018-11-06 11:23:39 +0200

    add solution and lambda

---
+++ Monitoring/Infrastructure/Monitoring.Infrastructure.MongoDB/Documents/MinerLogDocument.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Monitoring.Infrastructure.MongoDB.Documents
+{
+    public class MinerLogDocument
+    {
+        public string Id { get; set; }
+
+        public string MiningUnitId { get; set; }
+
+        public int HashRate { get; set; }
+
+        public double AvgTemp { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public bool HasMemoryError { get; set; }
+    }
+}
---
+++ Monitoring/Infrastructure/Monitoring.Infrastructure.MongoDB/Documents/MinerUnitDocument.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Monitoring.Infrastructure.MongoDB.Documents
+{
+    public class MinerUnitDocument
+    {
+        public string Id { get; set; }
+
+        public GPU GPU { get; set; }
+
+        public Motherboard MotherBoard { get; set; }
+
+        public Tuning TuningSettings { get; set; }
+
+        public BIOS BIOSSettings { get; set; }
+
+        public Coin Coin { get; set; }
+
+        public DateTime CreatedTimestamp { get; set; }
+    }
+
+    public class GPU
+    {
+        public string SysLabel { get; set; }
+
+        public string PCIESlotId { get; set; }
+
+        public string MacAddress { get; set; }
+
+        public bool HasRiser { get; set; }
+    }
+
+    public class Motherboard
+    {
+        public string SysLabel { get; set; }
+
+        public string MacAddress { get; set; }
+
+        public string SerialNumber { get; set; }
+    }
+
+    public class BIOS
+    {
+        public string MemoryStrapping { get; set; }
+        public string SettingsHash { get; set; }
+    }
+
+    public class Tuning
+    {
+        public int CPUClockSpeed { get; set; }
+
+        public int MemoryClockSpeed { get; set; }
+
+        public double CPUVoltage { get; set; }
+
+        public double VRAMVoltage { get; set; }
+    }
+
+    public class Coin
+    {
+        public string Name { get; set; }
+
+        public string Algorithm { get; set; }
+    }
+}
