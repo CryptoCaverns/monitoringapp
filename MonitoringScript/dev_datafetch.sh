1951cf2b1221f19ad6f9e54345d606b66c558209
psaik <Xotabu47@gmail.com>
2019-02-13 17:28:32 +0200

    Update dev_datafetch.sh

--- MonitoringScript/dev_datafetch.sh
+++ MonitoringScript/dev_datafetch.sh
@@ -1,49 +1,53 @@
 #!/bin/bash
-
+#set -x
 OUTPUTDIR="gpu_data"
 mkdir -p ./$OUTPUTDIR
 
+MOTHERBOARD_NAME=$(sudo /usr/sbin/dmidecode -t 2 | grep -Poi "(?<=Product\sName\:\s)(.*)")
+MOTHERBOARD_SERIALNUMBER=$(sudo /usr/sbin/dmidecode -t 2 | grep -Poi "(?<=Serial\sNumber\:\s)(.*)")
 
-MOTHERBOARD_NAME=$(/usr/sbin/dmidecode -t 2 | grep -Poi "(?<=Product\sName\:\s)(.*)")
-#MOTHERBOARD_MANUFACTURER=$(/usr/sbin/dmidecode -t 2 | grep -Poi "(?<=Manufacturer\:\s)(.*)")
-#MOTHERBOARD_VERSION=$(/usr/sbin/dmidecode -t 2 | grep -Poi "(?<=Version\:\s)(.*)")
-#MOTHERBOARD_SERIALNUMBER=$(/usr/sbin/dmidecode -t 2 | grep -Poi "(?<=Serial\sNumber\:\s)(.*)")
-MOTHERBOARD_MACADDRESS=$(ifconfig -a | grep -Eo 'HWaddr(\s+\S+){1}' | awk '{print$2}')
-MOTHERBOARD_IP=$(hostname -i | awk '{print $2}')
+#CPU_CLOCK_SPEED="$(lscpu | grep MHz | awk '{print$3}')MHz"
+CPU_CLOCK_SPEED="$(sudo dmidecode -t processor | grep Current | awk '{print $3}')MHz"
+CPU_VOLTAGE="$(sudo dmidecode -t processor | grep Voltage | awk '{print $2}')V"
+RAM_CLOCK_SPEED="$(sudo dmidecode --type 17 | grep Speed | grep MHz | head -n1 | awk '{print $2}')MHz"
+RAM_VOLTAGE="$(sudo dmidecode --type 17 | grep voltage | grep Configured | head -n1 | awk '{print $3}')V"
 
-GPU_CPU_CLOCK_SPEED=$(tail -1 /sys/class/drm/card0/device/pp_dpm_sclk | grep -Poi "(?<=\d\: )(\d+)")
-GPU_MEM_CLOCK_SPEED=$(tail -1 /sys/class/drm/card0/device/pp_dpm_mclk | grep -Poi "(?<=\d\: )(\d+)")
-GPU_VOLTAGE=$(sensors amdgpu-pci-010 | grep vddgfx | awk '{print$2}')
+BIOS_VERSION=$(sudo /usr/sbin/dmidecode -t 0 | grep -Poi "(?<=Version:\s)(.*)")
+BIOS_VENDOR=$(sudo /usr/sbin/dmidecode -t 0 | grep -Poi "(?<=Vendor:\s)(.*)")
 
-# AVG_TEMP=$(sensors amdgpu-pci-0100 | grep temp1 | awk '{print$2}')
-# TIME_STAMP=$(date)
+GPU_INFO=$(sudo /opt/ethos/bin/amdmeminfo -o -q -s)
 
-BIOS_VERSION=$(/usr/sbin/dmidecode -t 0 | grep -Poi "(?<=Version:\s)(.*)")
-BIOS_VENDOR=$(/usr/sbin/dmidecode -t 0 | grep -Poi "(?<=Vendor:\s)(.*)")
+TIME_STAMP=$(date)
 
-GPU_INFO=$(/opt/ethos/bin/amdmeminfo -o -q -s)
 
-GPU_INFO=$(/opt/ethos/bin/amdmeminfo -o -q -s)
-COUNTER=1
+COUNTER=0
+
  while read -r line; do
      GPU_NUMBER=$(echo $line | awk -F: '{print$1}')
      GPU_PCI=$(echo $line | awk -F: '{print$2}')
      GPU_TYPE=$(echo $line | awk -F: '{print$3}')
-     #GPU_MEMORY_TYPE=$(echo $line | awk -F: '{print$5}')
+     GPU_MEMORY_TYPE=$(echo $line | awk -F: '{print$5}')
+     PRODUCT_NAME=$(sudo atiflash -ai $COUNTER | grep Product | awk -F: '{print$2}')
+     CHECKSUM=$(sudo atiflash -cb $COUNTER | awk '{print $4}')
+     let COUNTER+=1
      JSON_STRING=$( jq -n \
                    --arg gn "$GPU_NUMBER" \
+                   --arg pn "$PRODUCT_NAME" \
+                   --arg cc "$CHECKSUM" \
                    --arg gp "$GPU_PCI" \
-                   --arg gt "$GPU_TYPE" \
-                   --arg gv "$GPU_VOLTAGE" \
-                   --arg gccs "$GPU_CPU_CLOCK_SPEED" \
-                   --arg gmcs "$GPU_MEM_CLOCK_SPEED" \
+                   --arg ts "$TIME_STAMP" \
                    --arg mn "$MOTHERBOARD_NAME" \
-                   --arg mi "$MOTHERBOARD_IP" \
-                   --arg mm "$MOTHERBOARD_MACADDRESS" \
+                   --arg ms "$MOTHERBOARD_SERIALNUMBER" \
+                   --arg ccp "$CPU_CLOCK_SPEED" \
+                   --arg cv "$CPU_VOLTAGE" \
+                   --arg rcs "$RAM_CLOCK_SPEED" \
+                   --arg rv "$RAM_VOLTAGE" \
                    --arg bver "$BIOS_VERSION" \
                    --arg bven "$BIOS_VENDOR" \
-                   '{GPU: {SysLabel: $gn, Name: $gt, PCIESlotId: $gp, HasRiser: false}, MotherBoard: {SysLabel: $mn, IPAddress: $mi, MacAddress: $mm}, TunningSettings: {CPUClockSpeed: $gccs, MemoryClockSpeed: $gmcs, Voltage: $gv}, BIOSSettings: { BiosVersion: $bver, BiosVendor: $bven, MemoryStrapping, }}' )
+                   '{GPU: {SysLabel: $gn, ProductName: $pn, PCIESlotId: $gp, MacAddress, HasRiser: false}, MotherBoard: {SysLabel: $mn, SerialNumber: $ms}, TunningSettings: {CPUClockSpeed: $ccp, MemoryClockSpeed: $rcs, CPUVoltage: $cv, VRAMVoltage: $rv, TimeStamp: $ts}, BIOSSettings: { BiosVersion: $bver, BiosVendor: $bven, BiosHash: $cc, MemoryStrapping, }}' )
       echo $JSON_STRING | jq . > ./$OUTPUTDIR/${GPU_NUMBER}.json
-      #aws s3 cp ./$OUTPUTDIR/${GPU_NUMBER}.json s3://monitoringapp-dev/rigs/rig1/
-     let COUNTER+=1
+      aws s3 cp ./$OUTPUTDIR/${GPU_NUMBER}.json s3://monitoringapp-dev/rigs/4fe55d/
  done <<< "$GPU_INFO"
+
+#MOTHERBOARD_MANUFACTURER=$(/usr/sbin/dmidecode -t 2 | grep -Poi "(?<=Manufacturer\:\s)(.*)")
+#MOTHERBOARD_VERSION=$(/usr/sbin/dmidecode -t 2 | grep -Poi "(?<=Version\:\s)(.*)")
