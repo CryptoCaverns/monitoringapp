13b99ca9c15619db7a18348706dce1dc86996008
roman.galetskyi <rg@evendo.com>
2019-07-03 18:51:58 +0300

    Fix stats script

--- MonitoringScript/minerunit_stats.sh
+++ MonitoringScript/minerunit_stats.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-OUTPUTDIR="logs"
+OUTPUTDIR="cc_mining/stats"
 mkdir -p ./$OUTPUTDIR
 
-
-#HASH=$(tail -n1 /var/run/ethos/miner_hashes.file)
 AVG_TEMP=$(sensors amdgpu-pci-0100 | grep temp1 | awk '{print$2}')
 TIME_STAMP=$(date)
-FILE_DATE=$(date +%Y_%m_%d_%H%M%S)
+FILE_DATE=$(date +%Y%m%d_%H%M%S)
 GPU_INFO=$(/opt/ethos/bin/amdmeminfo -o -q -s)
 
-COUNTER=0
- while read -r line; do
-     #GPU_NUMBER=$(echo $line | awk -F: '{print$1}')
-     PRODUCT_NAME=$(sudo atiflash -ai $COUNTER | grep Product | awk '{print$4}')
-     CHECKSUM=$(sudo atiflash -cb $COUNTER | awk '{print $4}')
-     let COUNTER+=1
-     HASH=$(tail -n1 /var/run/ethos/miner_hashes.file | awk -v num=$COUNTER '{print $num}')
+while read -r line; do
+     GPU=$(echo $line | awk -F: '{print$1}')
+	 GPU_NUMBER=${GPU#"GPU"}
+	
+     PRODUCT_NAME=$(sudo atiflash -ai $GPU_NUMBER | grep Product | awk '{print$4}')
+     CHECKSUM=$(sudo atiflash -cb $GPU_NUMBER | awk '{print $4}')
+     HASH=$(tail -n1 /var/run/ethos/miner_hashes.file | awk -v num=$$GPU_NUMBER '{print $num}')
      #GPU_MEMORY_TYPE=$(echo $line | awk -F: '{print$5}')
      JSON_STRING=$( jq -n \
                    --arg at "$AVG_TEMP" \
@@ -26,6 +24,6 @@ COUNTER=0
                    --arg cc "$CHECKSUM" \
                    '{GPU: {SysLabel: $pn, BiosHash: $cc, HashRate: $hr, AvgTemp: $at, TimeStamp: $ts, HasMemoryError: false}}' )
       echo $JSON_STRING | jq . > ./$OUTPUTDIR/${PRODUCT_NAME}_${FILE_DATE}.json
-      aws s3 cp ./$OUTPUTDIR/${PRODUCT_NAME}_${FILE_DATE}.json s3://monitoringapp-dev/logs/4fe55d/
+      MINER_UNIT_STATS_RESP=$(curl -vX POST -H "Content-Type: application/json" --data-binary @./$OUTPUTDIR/${PRODUCT_NAME}_${FILE_DATE}.json 'https://xaxiho2w86.execute-api.ca-central-1.amazonaws.com/Prod/api/monitoring/stats')
  done <<< "$GPU_INFO"
- rm -rf ./logs/*
+ rm -rf ./$OUTPUTDIR/*
